4ca2596162cc4af6d096eadb2ec7f196920361bf
Pien <pien_tian@aol.com>
2022-05-27 10:04:11 +0900

    add prompt for the case running program without argument

--- bodgeSiteUpDownChecker.ps1
+++ bodgeSiteUpDownChecker.ps1
@@ -1,11 +1,28 @@
-$Content = (Invoke-WebRequest https://www.isitdownrightnow.com/check.php?domain=$args).Content
-Write-Host "Website $args is ..." -NoNewLine
+if($args){
+$site = $args
+}
+else{
+$site = Read-Host("type the site domain")
+}
+
+
+$Content = (Invoke-WebRequest https://www.isitdownrightnow.com/check.php?domain=$site).Content
+Write-Host "Website $site is ..." -NoNewLine
 if (select-string -InputObject $Content -pattern 'UP' -Quiet){
 Write-Host " UP" -NoNewLine -ForeGroundColor Green
 }else{
 Write-Host " Down" -NoNewLine -ForeGroundColor Red
 Write-Host ""
-Write-Host "see details at https://www.isitdownrightnow.com/$args.html"
+Write-Host "see details at https://www.isitdownrightnow.com/$site.html"
 $string = $Content -replace '<[^>]+>',' '
 Write-Host $string
+}
+Write-Host ""
+Write-Host ""
+Write-Host "unreachable for 19130days mean you probably typed wrong domain"
+Write-Host ""
+
+if(!$args){
+
+Read-Host("To finish the program Hit Enter")
 }
